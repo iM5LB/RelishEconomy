6ec38b551ebedd2645d59a583429f97b41b9f495
GitHub Deploy <deploy@github.com>
2026-03-04 17:15:21 +0400

    docs: exclude markdown link labels from search index

--- docs/assets/docs.js
+++ docs/assets/docs.js
@@ -76,7 +76,7 @@ function stripMarkdown(md) {
     .replace(/```[\s\S]*?```/g, ' ')
     .replace(/`[^`]*`/g, ' ')
     .replace(/!\[[^\]]*]\([^)]+\)/g, ' ')
-    .replace(/\[([^\]]+)\]\([^)]+\)/g, '$1')
+    .replace(/\[[^\]]+\]\([^)]+\)/g, ' ')
     .replace(/^#{1,6}\s+/gm, '')
     .replace(/[>*_~\-\[\]\(\)]/g, ' ')
     .replace(/\s+/g, ' ')
